e5f698c1f10de5f4b06f1c7e4280e2a135537df9
AndjelijaMladenovic00 <andjelija.mladenovic@elfak.rs>
2022-08-16 20:21:19 +0200

    report module added, problem with dependencies

--- src/modules/report/report.module.ts
+++ src/modules/report/report.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthService } from 'src/auth/auth.service';
+import { Article } from 'src/entities/article.entity';
 import { Report } from 'src/entities/report.entity';
+import { User } from 'src/entities/user.entity';
+import { ArticleService } from '../article/article.service';
+import { UsersService } from '../users/users.service';
 import { ReportController } from './report.controller';
 import { ReportService } from './report.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Report])],
+  imports: [
+    TypeOrmModule.forFeature([Report]),
+    TypeOrmModule.forFeature([Article]),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [ReportController],
-  providers: [ReportService],
+  providers: [
+    ReportService,
+    AuthService,
+    JwtService,
+    ArticleService,
+    UsersService,
+  ],
 })
 export class ReportModule {}
--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Article } from 'src/entities/article.entity';
 import { Report } from 'src/entities/report.entity';
+import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class ReportService {
   constructor(
     @InjectRepository(Report) private reportRepository: Repository<Report>,
+    @InjectRepository(Article) private articleRepository: Repository<Article>,
+    @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
+
+  public createReport(articleId: number, userId: number) {}
 }
